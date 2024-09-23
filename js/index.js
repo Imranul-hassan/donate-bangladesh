612bf068f7ed5a8a972a1e6b018774f40a831824
Akib <imranulhassan3296@gmail.com>
2024-09-23 22:43:39 +0600

    added blog page and transaction system

--- js/index.js
+++ js/index.js
@@ -1,17 +1,60 @@
 
+// donation for noakhali
 const donateButton = document.getElementById('donate-button').addEventListener('click', function(){
     
-    const donateAmount = parseFloat(document.getElementById('donate-amount').value); 
-    const balance = parseFloat(document.getElementById('balance').innerText);
-    const totalBalance = parseFloat(document.getElementById('savings').innerText)
+    const donateAmount = parseFloat(getInputFieldById('donate-amount'));
 
-    const newBalance = balance + donateAmount;
-    const remainingSavings = totalBalance - donateAmount;
+    const balance = parseFloat(getInputInnerTextById('balance'));
+    const totalBalance = parseFloat(getInputInnerTextById('savings'));
+    const donateTitle = document.getElementById('donate-title').innerText;
 
-    const balanceOfNoakhali = document.getElementById('balance');
-    balanceOfNoakhali.innerText = newBalance;
+    if(!isNaN(donateAmount) && donateAmount > 0){
+        
+        const newBalance = balance + donateAmount;
+        const remainingSavings = totalBalance - donateAmount;
 
-    const afterDonateSavings = document.getElementById('savings');
-    afterDonateSavings.innerText = remainingSavings;
+        const balanceOfNoakhali = document.getElementById('balance');
+        balanceOfNoakhali.innerText = newBalance;
+
+        const afterDonateSavings = document.getElementById('savings');
+        afterDonateSavings.innerText = remainingSavings;
+        
+    } 
+    else{
+        alert('This is not a valid input')
+    }
+    
+    const historyItem = document.createElement('div');
+    historyItem.className = 'bg-white p-3 rounded-md'
+    historyItem.innerHTML =`
+    <p> ${donateAmount} Taka ${donateTitle}  </p>
+     <p> ${new Date().toLocaleString()} </p>
+
+    `;
+    const historyContainer = document.getElementById('history-list')
+    historyContainer.insertBefore(historyItem , historyContainer.firstChild)
    
+})
+
+// donation and history tab 
+
+const historyTab = document.getElementById('history-tab');
+const donationTab = document.getElementById('donation-tab')
+
+historyTab.addEventListener('click', function(){
+    historyTab.classList.add('bg-[#ABEF5F]');
+
+    donationTab.classList.remove('bg-[#ABEF5F]');
+
+    document.getElementById('donate-container').classList.add('hidden');
+    document.getElementById('history-container').classList.remove('hidden');
+})
+
+donationTab.addEventListener('click', function(){
+    donationTab.classList.add('bg-[#ABEF5F]');
+
+    historyTab.classList.remove('bg-[#ABEF5F]');
+
+     document.getElementById('history-container').classList.add('hidden');
+     document.getElementById('donate-container').classList.remove('hidden');
 })
---
+++ js/utilites.js
@@ -0,0 +1,12 @@
+
+// input for value
+function getInputFieldById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
+
+// input for innerText
+function getInputInnerTextById(id){
+    const inputValue = document.getElementById(id).innerText;
+    return inputValue;
+}
